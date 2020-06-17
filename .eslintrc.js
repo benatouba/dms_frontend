9491e4e69ed154fd204c797e45745b9de6d9ad33
Benjamin Schmidt <benschmidt@live.de>
2020-06-17 18:07:56 +0200

    minor

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,32 @@ module.exports = {
     env: {
         node: true,
     },
-    extends: ['plugin:vue/essential', 'eslint:recommended', 'plugin:prettier/recommended'],
+    extends: ['plugin:vue/essential', 'eslint:recommended'],
     parserOptions: {
         parser: 'babel-eslint',
+        ecmaVersion: 5,
+        sourceType: "module",
+        ecmaFeatures: {
+            "jsx": false
+        }
     },
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        quotes: [ "warn", "single" ],
+        semi: "off",
+        strict: 'off',
     },
+    overrides: [
+        {
+            // for testing with mocha
+            files: [
+                "**/__tests__/*.{j,t}s?(x)",
+                "**/tests/unit/**/*.spec.{j,t}s?(x)"
+            ],
+            env: {
+                "mocha": true
+            }
+        }
+    ]
 }
