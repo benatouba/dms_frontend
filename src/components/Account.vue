971a8eea1935c39f5d6a658054d26952ded09684
Benjamin Schmidt <benschmidt@live.de>
2020-06-18 19:15:39 +0200

    account view - part 2

---
+++ src/components/Account.vue
@@ -0,0 +1,71 @@
+<template>
+    <v-container fluid class="text--primary">
+        <h1 class="ma-5">{{ $t('account.title') }}</h1>
+            <v-card>
+                <v-card-title>
+                    <v-avatar class="ma-5">
+                        <v-icon right color="primary" x-large>mdi-account-circle</v-icon>
+                    </v-avatar>
+                    <span>{{ accountInfo[0].username}}</span>
+                </v-card-title>
+                <v-card-text
+                        v-for="(field, key) in getShowInfo(accountInfo[0])"
+                        :key="key"
+                >
+                    <v-row>
+                        <v-col dense
+                                class="text-no-wrap"
+                                cols="3"
+                                sm="3"
+                        >
+<!--                            NOTE: We user the register-components messages here to reduce duplication-->
+                            <p class="key"> {{ $t(`register.${key}`) }}</p>
+                        </v-col>
+                        <v-col
+                                sm="3"
+                                md="3"
+                        >
+                            <b>{{ field }}</b>
+                        </v-col>
+                    </v-row>
+                    <v-divider></v-divider>
+                </v-card-text>
+            </v-card>
+    </v-container>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+export default {
+    name: 'Account',
+    computed: {
+        ...mapActions({
+            getInfo: 'accounts/info'
+        }),
+    },
+    methods: {
+        handlePasswordChange() {
+        },
+        getShowInfo(data) {
+            let newDict = Object.assign({}, data)
+            delete newDict.groups
+            delete newDict.username
+            return newDict
+        },
+    },
+    async mounted() {
+        this.accountInfo = await this.getInfo
+    },
+    data() {
+        return {
+            accountInfo: null,
+        }
+    },
+}
+</script>
+
+<style>
+    .key{
+        text-transform: capitalize;
+    }
+</style>
---
+++ src/views/Account.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="home">
+        <v-container class="mx-4 primary--text">
+        <Account />
+        </v-container>
+    </div>
+</template>
+
+<script>
+    import Account from '@/components/Account'
+export default {
+    name: 'Account',
+}
+</script>
