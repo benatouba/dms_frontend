f5163f89e195541cc75ad6c5cd5a8086f9eb95f6
Benjamin Schmidt <benschmidt@live.de>
2020-07-08 18:40:54 +0200

    hook admin view

---
+++ src/views/Administration.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="home">
+        <v-container class="mx-4 primary--text">
+            <h2>{{ $t('admin.title') }}</h2>
+            <MetadataUpload />
+            <v-divider></v-divider>
+            <UserCards />
+        </v-container>
+    </div>
+</template>
+
+<script>
+import MetadataUpload from '../components/MetadataUpload'
+import UserCards from '../components/UserCards'
+export default {
+    name: 'AdministrationView',
+    components: {
+        MetadataUpload,
+        UserCards
+    }
+}
+</script>
