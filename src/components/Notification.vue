9c68be0d94273b176fbe11d9c3991c198b8d2b04
Benjamin Schmidt <benschmidt@live.de>
2020-07-09 13:33:11 +0200

    notification as banner and always visible

--- src/components/Notification.vue
+++ src/components/Notification.vue
@@ -1,7 +1,26 @@
 <template>
-    <v-alert v-if="alerts.message" border="left" :class="alerts.type">
-        {{ alerts.message }}
-    </v-alert>
+    <v-hover v-slot:default="{ hover }">
+        <v-banner
+                app
+                v-if="alerts.message"
+                :color="alerts.type"
+                rounded
+                sticky
+                :elevation="hover ? 24 : 5"
+                style="z-index: 10;"
+            >
+            <v-btn
+                    icon
+                    @click="clearAlert"
+                    rounded
+            >
+                <v-icon>
+                    mdi-close-circle
+                </v-icon>
+            </v-btn>
+            {{ alerts.message }}
+        </v-banner>
+    </v-hover>
 </template>
 
 <script>
@@ -17,13 +36,13 @@ export default {
     methods: {
         ...mapActions({
             clearAlert: 'alerts/clear',
-        }),
+        })
     },
     watch: {
         $route() {
             // clear alert on location change
             this.clearAlert()
         },
-    },
+    }
 }
 </script>
