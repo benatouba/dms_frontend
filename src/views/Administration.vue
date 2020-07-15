aded7bbf3f29b7bc71f5a14cb8ab0fec73084b59
Benjamin Schmidt <benschmidt@live.de>
2020-07-15 16:05:42 +0200

    use reusable file card component

--- src/views/Administration.vue
+++ src/views/Administration.vue
@@ -3,6 +3,7 @@
         <v-container class="mx-4 primary--text">
             <h2>{{ $t('admin.title') }}</h2>
             <MetadataUpload />
+            <FileCard v-for="file in meta" :key="file.name" :file="file"></FileCard>
             <v-divider></v-divider>
             <UserCards />
         </v-container>
@@ -12,11 +13,15 @@
 <script>
 import MetadataUpload from '../components/MetadataUpload'
 import UserCards from '../components/UserCards'
+import FileCard from "../components/FileCard";
+import { mapState } from "vuex";
 export default {
     name: 'AdministrationView',
     components: {
         MetadataUpload,
+        FileCard,
         UserCards
-    }
+    },
+    computed: mapState('upload', ['meta']),
 }
 </script>
