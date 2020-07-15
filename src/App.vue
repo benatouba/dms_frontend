07e7a41a89b03c2704621dd614c39659288a3f1a
Benjamin Schmidt <benschmidt@live.de>
2020-07-15 15:33:10 +0200

    adjust to new store structure

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
     <v-app class="primary">
         <Navbar />
         <v-main style="margin: auto; width: 80%;">
-            <LoginInfo v-if="!isLoggedIn && loginInfo" />
+            <LoginInfo v-if="!isLoggedIn && alerts.loginInfo" />
             <Notification />
             <router-view></router-view>
         </v-main>
@@ -14,19 +14,16 @@
 import Navbar from './components/Navbar'
 import LoginInfo from './components/LoginInfo'
 import Footer from './components/Footer'
-import { mapActions, mapState, mapGetters } from 'vuex'
+import { mapActions, mapState } from 'vuex'
 import Notification from './components/Notification';
 export default {
     name: 'App',
-    components: {Notification, Navbar, LoginInfo, Footer },
+    components: { Notification, Navbar, LoginInfo, Footer },
     computed: {
         ...mapState({
             alerts: state => state.alerts,
-        }),
-        ...mapGetters({
-            isLoggedIn: 'accounts/isLoggedIn',
-            loginInfo: 'alerts/loginInfo',
-        }),
+            isLoggedIn: state => state.accounts.status.isLoggedIn
+        })
     },
     methods: {
         ...mapActions({
