c80be02074078ba7c1d64e678fb1211cea9ac567
Benjamin Schmidt <benschmidt@live.de>
2020-07-23 14:34:18 +0200

    initial - simple link button with one slot

---
+++ src/components/BLinkButton.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-btn ripple x-small depressed :href="href" target="_blank">
+        <slot></slot>
+        <v-icon x-small>mdi-open-in-new</v-icon>
+    </v-btn>
+</template>
+
+<script>
+export default {
+    name: 'BLinkButton',
+    props: {
+        href: {
+            type: String,
+            required: true,
+        },
+    },
+}
+</script>>
