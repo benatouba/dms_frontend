e0cf2938a828a8219acf7521929041e00dbbe92f
Benjamin Schmidt <benschmidt@live.de>
2020-07-24 15:28:27 +0200

    remove item and internationalize role titles

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -2,11 +2,11 @@
     <div class="contact">
         <v-container class="mx-4 primary--text">
             <h1>Contact Info</h1>
-            <v-card v-for="contact in contacts" :key="contact.id" class="ma-1">
+            <v-card v-for="(contact, id) in contacts" :key="id" class="ma-1">
                 <v-container>
                     <v-row justify="space-between">
                         <v-col cols="auto">
-                            <div class="overline mt-1 ml-4">{{ contact.role }}</div>
+                            <div class="overline mt-1 ml-4">{{ contact.role[$i18n.locale] }}</div>
                             <v-card-title>
                                 {{ contact.name }}
                             </v-card-title>
@@ -46,43 +46,43 @@ export default {
             contacts: [
                 {
                     id: 0,
-                    name: 'Dieter Scherer',
-                    role: 'Programmkoordinator',
-                    website: 'http://www.klima.tu-berlin.de/index.php?show=team_scherer',
-                    email: 'dieter.scherer@tu-berlin.de',
-                    tel: '+49-(0)30-314-71356',
+                    name: 'Benjamin Schmidt',
+                    role: { de: 'DMS Support', en: 'DMS Support' },
+                    website: 'https://www.klima.tu-berlin.de/index.php?show=team_schmidt',
+                    email: 'benjamin.schmidt@tu-berlin.de',
+                    tel: '+49-(0)30-314-73195',
                 },
                 {
                     id: 1,
+                    name: 'Tom Grassmann',
+                    role: { de: 'DMS Support', en: 'DMS Support' },
+                    website: 'http://www.klima.tu-berlin.de/index.php?show=team_grassmann',
+                    email: 'grassmann@tu-berlin.de',
+                    tel: '+49-(0)30-314-71192',
+                },
+                // {
+                //     id: 2,
+                //     name: 'Katharina Scherber ',
+                //     role: 'Koordinationsbüro',
+                //     website: 'http://www.klima.tu-berlin.de/index.php?show=team_scherber',
+                //     email: 'katharina.scherber@tu-berlin.de',
+                //     tel: '+49-(0)30-314-71330',
+                // },
+                {
+                    id: 3,
                     name: 'Ute Fehrenbach   ',
-                    role: 'Koordinationsbüro',
+                    role: { de: 'Koordinationsbüro Berlin', en: 'Communications Office' },
                     website: 'http://www.klima.tu-berlin.de/index.php?show=team_fehrenbach',
                     email: 'ute.fehrenbach@tu-berlin.de',
                     tel: '+49-(0)30-314-71309',
                 },
                 {
-                    id: 2,
-                    name: 'Katharina Scherber ',
-                    role: 'Koordinationsbüro',
-                    website: 'http://www.klima.tu-berlin.de/index.php?show=team_scherber',
-                    email: 'katharina.scherber@tu-berlin.de',
-                    tel: '+49-(0)30-314-71330',
-                },
-                {
-                    id: 3,
-                    name: 'Tom Grassmann',
-                    role: 'DMS Support',
-                    website: 'http://www.klima.tu-berlin.de/index.php?show=team_grassmann',
-                    email: 'grassmann@tu-berlin.de',
-                    tel: '+49-(0)30-314-71192',
-                },
-                {
                     id: 4,
-                    name: 'Benjamin Schmidt',
-                    role: 'DMS Support',
-                    website: 'https://www.klima.tu-berlin.de/index.php?show=team_schmidt',
-                    email: 'benjamin.schmidt@tu-berlin.de',
-                    tel: '+49-(0)30-314-73195',
+                    name: 'Dieter Scherer',
+                    role: { de: 'Programmkoordinator', en: 'Program Coordinator' },
+                    website: 'http://www.klima.tu-berlin.de/index.php?show=team_scherer',
+                    email: 'dieter.scherer@tu-berlin.de',
+                    tel: '+49-(0)30-314-71356',
                 },
             ],
         }
