2110ed1f1941c5df5b52da6b56ac364d89f31e5c
Benjamin Schmidt <benschmidt@live.de>
2020-11-13 12:17:48 +0100

    add to commit 21a2cda540eb3b28c3f0490f44b1f741dd946245

--- src/components/BRadioButtonWithMenu.vue
+++ src/components/BRadioButtonWithMenu.vue
@@ -1,34 +1,40 @@
 <template>
-  <v-menu
-      rounded
-  >
-    <template v-slot:activator="{ attrs, on }">
-      <v-btn
-          icon
-          depressed
-          x-small
-          v-bind="attrs"
-          v-on="on"
+  <v-tooltip left>
+    <template v-slot:activator="{ on: onTooltip }">
+      <v-menu
+          rounded
+          v-on="onTooltip"
       >
-        <slot name="icon"></slot>
-      </v-btn>
-    </template>
-    <v-card>
-      <v-list dense>
-        <div
-            v-for="item in items"
-            :key="item.value"
-        >
-          <v-list-item
-              v-if="!!item.text"
-              @click="$emit('clickItem', item.value)"
+        <template v-slot:activator="{ attrs, on: onMenu }">
+          <v-btn
+              icon
+              depressed
+              x-small
+              v-bind="attrs"
+              v-on="{ ...onMenu, ...onTooltip }"
           >
-            <v-list-item-title>{{ item.text }}</v-list-item-title>
-          </v-list-item>
-        </div>
-      </v-list>
-    </v-card>
-  </v-menu>
+            <slot name="icon"></slot>
+          </v-btn>
+        </template>
+        <v-card>
+          <v-list dense>
+            <div
+                v-for="item in items"
+                :key="item.value"
+            >
+              <v-list-item
+                  v-if="!!item.text"
+                  @click="$emit('clickItem', item.value)"
+              >
+                <v-list-item-title>{{ item.text }}</v-list-item-title>
+              </v-list-item>
+            </div>
+          </v-list>
+        </v-card>
+      </v-menu>
+    </template>
+    <span>{{ tooltip }}</span>
+  </v-tooltip>
 </template>
 
 <script>
@@ -37,6 +43,10 @@ export default {
   props: {
     items: {
       type: Array,
+    },
+    tooltip: {
+      type: String,
+      default: '',
     }
   },
 }
