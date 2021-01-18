97efe93486e57e161b34deff59730b041d014f76
Benjamin Schmidt <benschmidt@live.de>
2021-01-18 11:30:52 +0100

    linting

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -3,7 +3,7 @@
         <v-container class="text--primary">
             <strong>{{ meta.institution }}</strong>
             <v-card>
-                <v-card-title style="margin: 0px;">
+                <v-card-title style="margin: 0px">
                     {{ $t('buttons.input') }}
                 </v-card-title>
                 <input
@@ -13,92 +13,92 @@
                     class="input text--secondary my-2"
                 />
                 <br />
-                <v-card-title style="font-size: 15px; margin: 0px;">
+                <v-card-title style="font-size: 15px; margin: 0px">
                     {{ $t('search.filter_options') }}
                 </v-card-title>
-              <v-card-text>
-                <v-row no-gutters>
-                  <v-col>
-                    <v-autocomplete
-                        dense
-                        v-model="options.acronym"
-                        :items="meta('institution')"
-                        item-text="ge_title"
-                        item-value="acronym"
-                        :label="$t('buttons.institution')"
-                        color="primary"
-                        clearable
-                        @change="handleSubmit"
-                    ></v-autocomplete>
-                    <v-autocomplete
-                        v-model="options.site__id"
-                        :items="meta('site')"
-                        item-text="site"
-                        item-value="id"
-                        :label="$t('buttons.site')"
-                        color="primary"
-                        @change="handleSubmit"
-                        dense
-                        clearable
-                    ></v-autocomplete>
-                    <v-autocomplete
-                        v-model="options.variables__variable"
-                        :items="meta('variable')"
-                        item-text="long_name"
-                        item-value="variable"
-                        :label="$t('buttons.variable')"
-                        color="primary"
-                        @change="handleSubmit"
-                        dense
-                        clearable
-                    ></v-autocomplete>
-                  </v-col>
-                </v-row>
-              </v-card-text>
-              <v-card-actions style="margin-top: -25px;">
-                <v-row justify="center" no-gutters>
-                  <v-switch
-                      @change="handleSubmit"
-                      color="primary"
-                      v-model="options.is_invalid"
-                      :label="$t('buttons.show_invalid')"
-                      :false-value="false"
-                      true-value="null"
-                  ></v-switch>
-                  <v-switch
-                      @change="handleSubmit"
-                      color="primary"
-                      v-model="options.is_old"
-                      :label="$t('buttons.show_old')"
-                      :false-value="false"
-                      true-value="null"
-                  ></v-switch>
-                  <v-switch
-                      v-if="account.token"
-                      @change="handleSubmit"
-                      color="primary"
-                      v-model="options.uploader"
-                      :label="$t('buttons.uploaded_by_me')"
-                      :true-value="account.id"
-                      :false-value="null"
-                  ></v-switch>
-                  <v-switch
-                      v-if="account.token"
-                      @change="handleSubmit"
-                      color="primary"
-                      v-model="options.acronym"
-                      :label="$t('buttons.uploaded_by_institution')"
-                      :true-value="account.institutions.join()"
-                      :false-value="null"
-                  ></v-switch>
-                </v-row>
-              </v-card-actions>
-              <v-card-actions>
-                <v-btn @click="handleSubmit" color="primary" large>
-                    <v-icon left dark>mdi-cloud-search</v-icon>
-                    {{ $t('buttons.search') }}
-                </v-btn>
-              </v-card-actions>
+                <v-card-text>
+                    <v-row no-gutters>
+                        <v-col>
+                            <v-autocomplete
+                                dense
+                                v-model="options.acronym"
+                                :items="meta('institution')"
+                                item-text="ge_title"
+                                item-value="acronym"
+                                :label="$t('buttons.institution')"
+                                color="primary"
+                                clearable
+                                @change="handleSubmit"
+                            ></v-autocomplete>
+                            <v-autocomplete
+                                v-model="options.site__id"
+                                :items="meta('site')"
+                                item-text="site"
+                                item-value="id"
+                                :label="$t('buttons.site')"
+                                color="primary"
+                                @change="handleSubmit"
+                                dense
+                                clearable
+                            ></v-autocomplete>
+                            <v-autocomplete
+                                v-model="options.variables__variable"
+                                :items="meta('variable')"
+                                item-text="long_name"
+                                item-value="variable"
+                                :label="$t('buttons.variable')"
+                                color="primary"
+                                @change="handleSubmit"
+                                dense
+                                clearable
+                            ></v-autocomplete>
+                        </v-col>
+                    </v-row>
+                </v-card-text>
+                <v-card-actions style="margin-top: -25px">
+                    <v-row justify="center" no-gutters>
+                        <v-switch
+                            @change="handleSubmit"
+                            color="primary"
+                            v-model="options.is_invalid"
+                            :label="$t('buttons.show_invalid')"
+                            :false-value="false"
+                            true-value="null"
+                        ></v-switch>
+                        <v-switch
+                            @change="handleSubmit"
+                            color="primary"
+                            v-model="options.is_old"
+                            :label="$t('buttons.show_old')"
+                            :false-value="false"
+                            true-value="null"
+                        ></v-switch>
+                        <v-switch
+                            v-if="account.token"
+                            @change="handleSubmit"
+                            color="primary"
+                            v-model="options.uploader"
+                            :label="$t('buttons.uploaded_by_me')"
+                            :true-value="account.id"
+                            :false-value="null"
+                        ></v-switch>
+                        <v-switch
+                            v-if="account.token"
+                            @change="handleSubmit"
+                            color="primary"
+                            v-model="options.acronym"
+                            :label="$t('buttons.uploaded_by_institution')"
+                            :true-value="account.institutions.join()"
+                            :false-value="null"
+                        ></v-switch>
+                    </v-row>
+                </v-card-actions>
+                <v-card-actions>
+                    <v-btn @click="handleSubmit" color="primary" large>
+                        <v-icon left dark>mdi-cloud-search</v-icon>
+                        {{ $t('buttons.search') }}
+                    </v-btn>
+                </v-card-actions>
             </v-card>
         </v-container>
     </div>
@@ -108,7 +108,7 @@
 import { mapActions, mapGetters, mapState } from 'vuex'
 // import DatePicker from "./DatePicker";
 export default {
-    name: 'search',
+    name: 'Search',
     components: {
         // DatePicker,
     },
@@ -187,7 +187,7 @@ export default {
                 uploader: null,
                 is_invalid: false,
                 is_old: false,
-                offset: 0
+                offset: 0,
             },
         }
     },
