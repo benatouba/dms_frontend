13ed8cc89d64eed3e52c1185a826a3e137644a69
Benjamin Schmidt <benschmidt@live.de>
2021-01-20 16:45:21 +0100

    linting

--- src/components/BConfirmationDialog.vue
+++ src/components/BConfirmationDialog.vue
@@ -1,87 +1,75 @@
 <template>
-  <v-dialog
-      v-model="dialog"
-      persistent
-      max-width="290"
-  >
-    <template v-slot:activator="{ on, attrs }">
-        <v-btn
-            class="mx-1"
-            :loading="loading"
-            :color="color"
-            :disabled="disabled"
-            v-bind="attrs"
-            v-on="on"
-        >
-          <slot></slot>
-        </v-btn>
-    </template>
-    <v-card>
-      <slot name="title">
-      <v-card-title class="headline">
-        {{ title }}
-      </v-card-title>
-      </slot>
-      <slot name="text">
-        <v-card-text>
-          {{ body }}
-        </v-card-text>
-      </slot>
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn
-            color="secondary"
-            text
-            @click="dialog=false">
-          {{ $t('buttons.cancel') }}
-        </v-btn>
-        <slot name="confirmation">
-          <v-btn
-              color="primary"
-              text
-              @click="$emit('confirm'); dialog=false"
-          >
-            {{ $t('buttons.confirm') }}
-          </v-btn>
-        </slot>
-      </v-card-actions>
-    </v-card>
-  </v-dialog>
+    <v-dialog v-model="dialog" persistent max-width="290">
+        <template #activator="{ on, attrs }">
+            <v-btn class="mx-1" :loading="loading" :color="color" :disabled="disabled" v-bind="attrs" v-on="on">
+                <slot></slot>
+            </v-btn>
+        </template>
+        <v-card>
+            <slot name="title">
+                <v-card-title class="headline">
+                    {{ title }}
+                </v-card-title>
+            </slot>
+            <slot name="text">
+                <v-card-text>
+                    {{ body }}
+                </v-card-text>
+            </slot>
+            <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="secondary" text @click="dialog = false">
+                    {{ $t('buttons.cancel') }}
+                </v-btn>
+                <slot name="confirmation">
+                    <v-btn
+                        color="primary"
+                        text
+                        @click="
+                            $emit('confirm')
+                            dialog = false
+                        "
+                    >
+                        {{ $t('buttons.confirm') }}
+                    </v-btn>
+                </slot>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
 </template>
 
 <script>
 export default {
-  name: "BConfirmationDialog",
-  props: {
-    disabled: {
-      type: Boolean,
-      default: false,
+    name: 'BConfirmationDialog',
+    props: {
+        disabled: {
+            type: Boolean,
+            default: false,
+        },
+        color: {
+            type: String,
+            default: 'primary',
+        },
+        title: {
+            type: String,
+            default: 'Confirmation needed',
+        },
+        body: {
+            type: String,
+            default: 'Are you sure, you want to do this?',
+        },
+        loading: {
+            type: Boolean,
+            default: false,
+        },
     },
-    color: {
-      type: String,
-      default: 'primary'
+    data() {
+        return {
+            dialog: false,
+        }
     },
-    title: {
-      type: String,
-      default: 'Confirmation needed'
-    },
-    body: {
-      type: String,
-      default: 'Are you sure, you want to do this?'
-    },
-    loading: {
-      type: Boolean,
-      default: false,
-    }
-  },
-  data() {
-    return {
-      dialog: false,
-    }
-  }
 }
 </script>
 
-<style scoped>
+<style scoped></style>
 
-</style>
