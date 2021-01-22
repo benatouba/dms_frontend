ecf576196cd91250cae8c6104138bafc42ed3da0
Benjamin Schmidt <benschmidt@live.de>
2021-01-22 12:42:18 +0100

    nameCheck and createPalmjob
    - ##store actions and mutations for the following
    
    - service function to request a filename check from backend, for files to
    upload.
    - service function to create a palmjob that will hold palmfiles
    - altered upload service func to be able to handle palmfiles as well

--- src/services/upload.service.js
+++ src/services/upload.service.js
@@ -1,10 +1,39 @@
 import authHeader from '../helpers/authentication'
 
+async function createPalmjob(job) {
+    const requestOptions = authHeader('POST')
+    requestOptions.headers.append('Content-Type', 'application/json')
+    requestOptions.body = JSON.stringify(job)
+    requestOptions.redirect = 'follow'
+    let resp = await fetch(process.env.VUE_APP_API_ENDPOINT + '/data/palmjob/', requestOptions)
+    if (resp.status === 201) {
+        return await resp.json()
+    } else {
+        throw await resp.json()
+    }
+}
+
+async function requestNameCheck(files) {
+    const requestOptions = authHeader('POST')
+    requestOptions.headers.append('Content-Type', 'application/json')
+    requestOptions.body = JSON.stringify(files)
+
+    const resp = await fetch(process.env.VUE_APP_API_ENDPOINT + '/data/palmfile/palmfilenames/', requestOptions)
+    // if (resp.status === 200) {
+    return resp
+    // } else {
+    //     throw await resp.json()
+    // }
+}
+
 function upload({ file, ignore_warnings, ignore_errors }) {
     const requestOptions = authHeader('POST')
 
     const formdata = new FormData()
-    formdata.append('file_type', 'UC2')
+    if (file.job) {
+        formdata.append('job', file.job)
+    }
+    // formdata.append('file_type', file.type.toUpperCase())
     formdata.append('file', file)
     if (ignore_warnings) {
         formdata.append('ignore_warnings', 'true')
@@ -12,10 +41,18 @@ function upload({ file, ignore_warnings, ignore_errors }) {
     if (ignore_errors) {
         formdata.append('ignore_errors', 'true')
     }
+    let path = '/data/'
+    if (file.db_filetype === 'uc2') {
+        path += 'file/'
+    } else if (file.db_filetype === 'palmfile') {
+        path += file.db_filetype + '/'
+    } else {
+        throw new Error('File not found')
+    }
 
     requestOptions.body = formdata
     requestOptions.redirect = 'follow'
-    return fetch(process.env.VUE_APP_API_ENDPOINT + `/data/file/`, requestOptions)
+    return fetch(process.env.VUE_APP_API_ENDPOINT + path, requestOptions)
 }
 
 function uploadMetadataList(obj) {
@@ -31,6 +68,8 @@ function uploadMetadataList(obj) {
 }
 
 export const uploadService = {
+    createPalmjob,
+    requestNameCheck,
     upload,
     uploadMetadataList,
 }
