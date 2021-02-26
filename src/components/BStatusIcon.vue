2f6b99e56564dcc5a7336e7903efede3ec3f06f9
Benjamin Schmidt <benschmidt@live.de>
2021-02-26 13:20:02 +0100

    linting and default

--- src/components/BStatusIcon.vue
+++ src/components/BStatusIcon.vue
@@ -1,41 +1,33 @@
 <template>
-  <v-tooltip
-      top
-  >
-    <template v-slot:activator="{ on, attrs }">
-      <v-icon
-          :color="color"
-          small
-          depressed
-          v-bind="attrs"
-          v-on="on"
-      >
-        {{ icon }}
-      </v-icon>
-    </template>
-    <span v-if="!!tooltip">{{ tooltip }}</span>
-  </v-tooltip>
+    <v-tooltip top>
+        <template #activator="{ on, attrs }">
+            <v-icon :color="color" small depressed v-bind="attrs" v-on="on">
+                {{ icon }}
+            </v-icon>
+        </template>
+        <span v-if="!!tooltip">{{ tooltip }}</span>
+    </v-tooltip>
 </template>
 
 <script>
 export default {
-  name: "BStatusIcon",
-  props: {
-    color: {
-      type: String,
-      default: 'secondary',
+    name: 'BStatusIcon',
+    props: {
+        color: {
+            type: String,
+            default: 'secondary',
+        },
+        icon: {
+            type: String,
+            default: 'mdi-exclamation',
+        },
+        tooltip: {
+            type: String,
+            default: '',
+        },
     },
-    icon: {
-      type: String,
-    },
-    tooltip: {
-      type: String,
-      default: '',
-    }
-  }
 }
 </script>
 
-<style scoped>
+<style scoped></style>
 
-</style>
